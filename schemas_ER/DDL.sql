a81700938c0d0b894dd9ae013a3b9e6c6ae12377
gurumuruganl <guru292001@gmail.com>
2025-05-18 22:59:18 +0530

    Updated DDL

--- schemas_ER/DDL.sql
+++ schemas_ER/DDL.sql
@@ -6,22 +6,20 @@
 -- customers definition
 
 CREATE TABLE `customers` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `customer_id` varchar(100) DEFAULT NULL,
+  `customer_id` varchar(100) NOT NULL,
   `customer_name` varchar(100) DEFAULT NULL,
   `customer_email` varchar(100) DEFAULT NULL,
   `customer_address` varchar(200) DEFAULT NULL,
   `createdDate` datetime DEFAULT NULL,
   `UpdatedDate` datetime DEFAULT NULL,
-  PRIMARY KEY (`id`),
+  PRIMARY KEY (`customer_id`),
   UNIQUE KEY `customers_unique` (`customer_id`)
 )
 
 -- Orders definition
 
 CREATE TABLE `Orders` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `order_id` varchar(25) DEFAULT NULL,
+  `order_id` varchar(25) NOT NULL,
   `product_id` varchar(50) DEFAULT NULL,
   `customer_id` varchar(50) DEFAULT NULL,
   `region_id` int(11) DEFAULT NULL,
@@ -32,22 +30,28 @@ CREATE TABLE `Orders` (
   `payment_method` varchar(50) DEFAULT NULL,
   `createdDate` datetime DEFAULT NULL,
   `updatedDate` datetime DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) 
+  `id` int(11) DEFAULT NULL,
+  PRIMARY KEY (`order_id`),
+  KEY `Orders_customers_FK` (`customer_id`),
+  KEY `Orders_products_FK` (`product_id`),
+  KEY `Orders_region_FK` (`id`),
+  CONSTRAINT `Orders_customers_FK` FOREIGN KEY (`customer_id`) REFERENCES `customers` (`customer_id`),
+  CONSTRAINT `Orders_products_FK` FOREIGN KEY (`product_id`) REFERENCES `products` (`product_id`),
+  CONSTRAINT `Orders_region_FK` FOREIGN KEY (`id`) REFERENCES `region` (`id`)
+)
 
 -- dev.products definition
 
 CREATE TABLE `products` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `product_id` varchar(50) DEFAULT NULL,
+  `product_id` varchar(50) NOT NULL,
   `product_name` varchar(50) DEFAULT NULL,
   `unit_price` decimal(10,2) DEFAULT NULL,
   `product_category` varchar(50) DEFAULT NULL,
   `createdDate` datetime DEFAULT NULL,
   `UpdatedDate` datetime DEFAULT NULL,
-  PRIMARY KEY (`id`),
+  PRIMARY KEY (`product_id`),
   UNIQUE KEY `products_unique` (`product_id`)
-)
+) 
 
 -- dev.region definition
 
